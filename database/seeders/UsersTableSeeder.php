7e2420c8adfb85fd68bc5d146810df806cc38218
coderkyo <devsuika@gmail.com>
2021-01-29 18:04:11 +0900

    管理员可删除用户

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -7,19 +7,15 @@ use App\Models\User;
 
 class UsersTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
-        //
         User::factory()->count(50)->create();
 
         $user = User::find(1);
         $user->name = 'Summer';
         $user->email = 'summer@example.com';
+        $user->password = bcrypt('password');
+        $user->is_admin = true;
         $user->save();
     }
 }
