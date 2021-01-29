ca65d7fdf9c6caabfdaf99fd43842a1c354cfab8
coderkyo <devsuika@gmail.com>
2021-01-29 13:55:34 +0900

    访问策略

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,6 +10,16 @@ use Auth;
 
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth', [
+            'except' => ['show', 'create', 'store']
+        ]);
+
+        $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+    }
     public function create()
     {
         return view('users.create');
@@ -38,10 +48,12 @@ class UsersController extends Controller
     }
     public function edit(User $user)
     {
+        $this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
     public function update(User $user, Request $request)
     {
+        $this->authorize('update', $user);
         $this->validate($request, [
             'name' => 'required|max:50',
             'password' => 'required|confirmed|min:6'
@@ -58,6 +70,11 @@ class UsersController extends Controller
 
         return redirect()->route('users.show', $user->id);
     }
-
+    public function __construct()
+    {
+        $this->middleware('auth', [            
+            'except' => ['show', 'create', 'store']
+        ]);
+    }
 
 }
